15ee5a6a28339b40bb4ef44d07bcc4e86958d201
Nemanja Drakulovic <drdrakula96@hotmail.com>
2017-10-22 03:04:51 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,12 @@
-def reverse_each_word(string)
-  array1 = []
-  string = string.split()
-  string.collect do |word|
-    array1.push(word.reverse)
-  end
-  array1.join(" ")
-end
+# def reverse_each_word(string)
+#   array1 = []
+#   string = string.split()
+#   string.collect do |word|
+#     array1.push(word.reverse)
+#   end
+#   array1.join(" ")
+# end
 
-string = "Yo, man my name is eminem"
 #
 # array1 = []
 # string = string.split()
@@ -16,4 +15,10 @@ string = "Yo, man my name is eminem"
 # end
 # array1.join(" ")
 
+
+
+def reverse_each_word(string)
+  string.split().collect {|word| word.reverse}.join(" ")
+end
+string = "Yo, man my name is eminem"
 puts reverse_each_word(string)
